bf483f991e0deff55edd350914145d12b7632008
elisuh <elisaverna@gmail.com>
2016-04-03 16:12:27 -0400

    initial commit

--- js/app.js
+++ js/app.js
@@ -12,6 +12,77 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	'use strict';
+  	//define variables
+
+
+  	// start new game
+
+  	function newGame() {
+  		//code 
+  	}
+
+  	// generate random number
+
+  	function generateRandom() {
+  		return Math.floor(Math.random() * 100) + 1;
+  	}
+
+  	// retrieve user's guess
+
+  	function getGuess() {
+  		var guess = document.getElementById("userGuess").elements[0].value;
+  	}
+
+  	// validate user's guess
+
+  	function checkGuess(guess) {
+  		var guess = parseFloat(guess);
+  		var differece;
+  		if (guess == number){
+		$("#feedback").text("You win!");
+	} else if (guess > number) {
+		difference = guess - number;
+	} else (guess < number) {
+		difference = number - guess;
+
+	}
+
+	if (difference >= 1 && difference <= 10) {
+		$("#feedback").text("Hotter");
+	} else if (difference < 10 && difference <= 25){
+		$("#feedback").text("Hot");
+	} else if (difference < 25 && difference <= 35){
+		$("#feedback").text("Warm");
+	} else if (difference < 35 && difference <= 50){
+		$("#feedback").text("Cold");
+	} else ( difference > 50){
+		$("#feedback").text("Freezing");
+	}
+
+
+};
+
+  	}
+
+  	// count number of guesses
+
+  	function counter() {
+  		count++;
+  	}
+
+  	// if..else statement that returns hot or cold
+
+  	// winner
+   
+   	// reset game
+
+   	function resetVar() {
+   		count = 0;
+   	}
+
+  
+
 });
 
 
