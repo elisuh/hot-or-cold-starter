13e5469da7e6dbf6b25737061db83ae903b7e032
elisuh <elisaverna@gmail.com>
2016-04-06 20:22:12 -0400

    changes to checkguess

--- js/app.js
+++ js/app.js
@@ -1,48 +1,51 @@
 var number = generateNumber();
 var count = 0;
-// generate random number
 
+// return generated random number
 function generateNumber() {
   return Math.floor(Math.random() * 100) + 1;
 }
 
-// retrieve user's guess
-
+// returning user's guess
 function getGuess() {
-	return document.getElementById("userGuess").value;
+	return document.getElementById('userGuess').value;
 }
 
 // validate user's guess and return hot or cold
-
 function checkGuess() {
-  var guess = parseFloat(getGuess()); // convert guess from string to number
-  var difference;
-  if (guess == number) {
-    return $("#feedback").innerHTML("You win!"); // if guess is the same as random number, you win!
+  var guess = getGuess(); // convert guess from string to number
+  var difference = 0;
+  
+  console.log(guess);
+  
+  if (guess === number) {
+    return $("#feedback").text("You win!"); // if guess is the same as random number, you win!
   } else if (guess > number) {
     difference = guess - number; // calculate difference if guess is great than number
   } else if (guess < number) {
     difference = number - guess; // calculate different if guess is less than number
   }
+  
+  // console.log(guess);
+  console.log(difference);
+  
   if (difference >= 1 && difference <= 5) {
-    $("#feedback").innerHTML("On fire!"); // if calculated difference is 1-5 away from guess, you're close
+    $("#feedback").text("On fire!"); // if calculated difference is 1-5 away from guess, you're close
   } else if (difference > 5 && difference <= 15) {
-    $("#feedback").innerHTML("Hotter!"); // if diff btwn 6 and 15, hotter
+    $("#feedback").text("Hotter!"); // if diff btwn 6 and 15, hotter
   } else if (difference > 15 && difference <= 30) {
-    $("#feedback").innerHTML("Hot"); // if between 15 and 30, hot
+    $("#feedback").text("Hot"); // if between 15 and 30, hot
   } else if (difference > 30 && difference <= 45) {
-    $("#feedback").innerHTML("Warm"); // if between 30 and 45, warm
+    $("#feedback").text("Warm"); // if between 30 and 45, warm
   } else if (difference > 45 && difference <= 65) {
-    $("#feedback").innerHTML("Cold"); // if between 45 and 65, cold
+    $("#feedback").text("Cold"); // if between 45 and 65, cold
   } else if (difference > 65) {
-    $("#feedback").innerHTML("Freezing"); // if difference is over 65, freezing
+    $("#feedback").text("Freezing"); // if difference is over 65, freezing
   }
 }
 
 // count number of guesses
 function countGuess() {
-  // define count variable
-  // var count = 0;
   count++; // increse count by 1
   $('#count').text(count); // and add text to #count id
 }
@@ -77,19 +80,16 @@ $(document).ready(function() {
 
   // reset game
 
-  $(".new").on('click', function() { // when new game button is clicked do the following
+  $(".new").on('click', function(e) {// when new game button is clicked do the following
+    e.preventDefault();
     // Empty Guesses
     $('#guessList').innerHTML = ""; // empty listguess field
-    
     // Reset Guess number on page
-    $('#count').innerHTML = "0";
-    
+    $('#count').text = "0";
     //Reset Guess Count in javascript
     count = 0;
-    
     // Feedback needs to equal Guess
-      $('#feedback').innerHTML = "Make your Guess!";
-    
+    $('#feedback').innerHTML = "Make your Guess!";
     // Generate New Random Number
     // var number = generateNumber();
     number = generateNumber(); //generate a new random number
